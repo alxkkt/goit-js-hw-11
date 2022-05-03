162a58c74f61be97c1117395b22c4dd870c8fa49
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-05-03 16:42:19 +0200

    added infinite scroll and simplelightbox

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { PicsServiceApi } from './js/fetchQuery';
 import { refs } from './js/refs';
 
 refs.form.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
-
-refs.loadMoreBtn.classList.add('visually-hidden');
+window.addEventListener('scroll', () => {
+  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+    onLoadMore();
+  }
+});
 
 const picsServiceApi = new PicsServiceApi();
 
@@ -27,24 +29,24 @@ function onSearch(e) {
     }
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
     cleanMarkup();
-    refs.gallery.insertAdjacentHTML('beforeend', createCardMarkup(hits));
-    refs.loadMoreBtn.classList.remove('visually-hidden');
-    // let lightbox = new SimpleLightbox('.gallery a');
+    refreshMarkup(hits);
   });
 }
 
 function onLoadMore() {
   picsServiceApi.fetchQuery().then(({ hits }) => {
     if (hits.length < 40) {
-      refs.loadMoreBtn.classList.add('visually-hidden');
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
     }
-
-    refs.gallery.insertAdjacentHTML('beforeend', createCardMarkup(hits));
-    // let lightbox = new SimpleLightbox('.gallery a');
-    // lightbox.refresh();
+    refreshMarkup(hits);
   });
 }
 function cleanMarkup() {
   refs.gallery.innerHTML = '';
+}
+
+function refreshMarkup(data) {
+  refs.gallery.insertAdjacentHTML('beforeend', createCardMarkup(data));
+  let lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
 }
